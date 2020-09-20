5178d16c85e97e05caf315bfa5456542423c45c6
Daniel Ngo <danielngo1998@gmail.com>
2020-09-20 09:58:44 +0700

    Init Commit

---
+++ src/test/java/io/locngo/ppm/tool/backend/PpmToolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.locngo.ppm.tool.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
